e07dcce79609baa9cd5e2c27b6c23691e61f51b7
Mark Woosey <dev@mrkw.se>
2017-02-18 18:34:42 +0000

    Actually fixing spaces for GitHub

--- tests/testlife.py
+++ tests/testlife.py
@@ -5,20 +5,20 @@ class testLife(unittest.TestCase):
 	# Scenario 0,5
 	def test_no_interaction(self):
 		emptyState = [  [0,0,0],
-				        [0,0,0],
-				        [0,0,0]  ]
+                        [0,0,0],
+                        [0,0,0]  ]
 		testLife = Life()
 		self.assertEqual(emptyState, testLife.evolve(emptyState))
 
 	# Scenario 1
 	def test_underpopulation(self):
 		startState = [ [1,0,0],
-					   [0,0,0],
-					   [0,0,1] ]
+                       [0,0,0],
+                       [0,0,1] ]
 
 		endState =   [ [0,0,0],
-					   [0,0,0],
-					   [0,0,0] ]
+                       [0,0,0],
+                       [0,0,0] ]
 
 		testLife = Life()
 		self.assertEqual(endState, testLife.evolve(startState))
@@ -26,12 +26,12 @@ class testLife(unittest.TestCase):
 	# Scenario 2
 	def test_overcrowding(self):
 		startState = [ [1,1,1],
-		 			   [1,1,1],
-					   [1,1,1] ]
+                       [1,1,1],
+                       [1,1,1] ]
 
 		endState =   [ [1,0,1],
-					   [0,0,0],
-					   [1,0,1] ]
+                       [0,0,0],
+                       [1,0,1] ]
 
 		testLife = Life()
 		self.assertEqual(endState, testLife.evolve(startState))
@@ -39,12 +39,12 @@ class testLife(unittest.TestCase):
 	# Scenario 3
 	def test_survival(self):
 		startState = [ [0,1,0],
-					   [1,0,1],
-					   [0,1,1] ]
+                       [1,0,1],
+                       [0,1,1] ]
 
 		endState =   [ [0,1,0],
-					   [1,0,1],
-					   [0,1,1] ]
+                       [1,0,1],
+                       [0,1,1] ]
 
 		testLife = Life()
 		self.assertEqual(endState, testLife.evolve(startState))
@@ -52,12 +52,12 @@ class testLife(unittest.TestCase):
 	# Scenario 4
 	def test_creation(self):
 		startState = [ [0,1,0],
-					   [1,1,1],
-					   [0,1,0] ]
+                       [1,1,1],
+                       [0,1,0] ]
 
 		endState =   [ [1,1,1],
-					   [1,0,1],
-					   [1,1,1] ]
+                       [1,0,1],
+                       [1,1,1] ]
 
 		testLife = Life()
 		self.assertEqual(endState, testLife.evolve(startState))
@@ -65,16 +65,16 @@ class testLife(unittest.TestCase):
 	# Scenario 6 & larger 2D arrays
 	def test_seeded_outcome(self):
 		startState = [ [0,0,0,0,0],
-		 			   [0,0,0,0,0],
-					   [0,1,1,1,0],
-					   [0,0,0,0,0],
-					   [0,0,0,0,0] ]
+                       [0,0,0,0,0],
+                       [0,1,1,1,0],
+                       [0,0,0,0,0],
+                       [0,0,0,0,0] ]
 
 		nextState =  [ [0,0,0,0,0],
-		 			   [0,0,1,0,0],
-					   [0,0,1,0,0],
-					   [0,0,1,0,0],
-					   [0,0,0,0,0] ]
+                       [0,0,1,0,0],
+                       [0,0,1,0,0],
+                       [0,0,1,0,0],
+                       [0,0,0,0,0] ]
 
 		testLife = Life()
 		self.assertEqual(nextState, testLife.evolve(startState))
@@ -82,12 +82,12 @@ class testLife(unittest.TestCase):
 
 	def test_non_square(self):
 		startState = [ [0,0,0,0,0],
-					   [0,1,1,1,0],
-					   [0,0,0,0,0] ]
+                       [0,1,1,1,0],
+                       [0,0,0,0,0] ]
 
 		nextState =  [ [0,0,1,0,0],
-					   [0,0,1,0,0],
-					   [0,0,1,0,0]]
+                       [0,0,1,0,0],
+                       [0,0,1,0,0]]
 
 		testLife = Life()
 		self.assertEqual(nextState, testLife.evolve(startState))
