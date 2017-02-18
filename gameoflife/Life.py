9a904b89f77be63c63c27d0d1a79711aa7bfb530
Mark Woosey <dev@mrkw.se>
2017-02-18 17:48:25 +0000

    Corrections to Life.py and complete test set (as per scenarios).
    * Life.py issues with array referencing/duplication resolved with
      initialisation of new array.
    * 4 additional tests written for Scenarios 1:4,6
    
    TODO: Comments, potentially further tests

--- gameoflife/Life.py
+++ gameoflife/Life.py
@@ -6,12 +6,24 @@ class Life(object):
 	def evolve(self,state):
 		# raise NotImplementedError("Please implement the 'evolve' method")
 
-		state_out = state[:]
+
+		# Initialize array of arrays of zeroes of equal dimension to states
+		# array.
+
+		state_out = []
+
+		for row in state:
+			state_out.append([])
+			for cell in row:
+				state_out[-1].append(0)
 
 		y = 0
-		x = 0
 
+		# Iterate through every row of state
 		while y < len(state):
+			x = 0
+
+			# Iterate through every cell of every row
 			while x < len(state):
 				state_out[y][x] = self.setCell(state,[y,x])
 				x += 1
@@ -26,37 +38,56 @@ class Life(object):
 	def setCell(self,state,current):
 		neighbours = 0
 
-		if (current[0] == (0 or len(state)) and (current[1] == (0 or len(state[0])))):
-			to_check = 3
-		elif (current[0] == (0 or len(state)) ^ (current[1] == (0 or len(state[0])))):
-			to_check = 5
-		else:
-			to_check = 8
-		neighbour_subset = [[current[0] - 1, current[0] + 1],
-						    [current[1] - 1, current[1] + 1]]
+		# if (current[0] == (0 or len(state)) and (current[1] == (0 or len(state[0])))):
+		# 	to_check = 3
+		# elif (current[0] == (0 or len(state)) ^ (current[1] == (0 or len(state[0])))):
+		# 	to_check = 5
+		# else:
+		# 	to_check = 8
+		neighbour_subset = [[current[0] - 1, current[0] + 2],
+						    [current[1] - 1, current[1] + 2]]
 
 		# Ensure lower bound isn't negative (slice would go to end of array)
 		for axis in neighbour_subset:
 			if axis[0] < 0:
 				axis[0] = 0
 
-		if neighbour_subset[0][1] > (len(state) - 1):
-			neighbour_subset[0][1] = (len(state) - 1)
+		if neighbour_subset[0][1] > (len(state)):
+			neighbour_subset[0][1] = (len(state))
 
-		if neighbour_subset[1][1] > (len(state[0]) - 1):
-			neighbour_subset[1][1] = (len(state[0]) - 1)
+		if neighbour_subset[1][1] > (len(state[0])):
+			neighbour_subset[1][1] = (len(state[0]))
 
+		# Iterate through every neighbouring row
 		for row in state[neighbour_subset[0][0]:neighbour_subset[0][1]]:
+			# Iterate through every neighbouring cell of a row
 			for cell in row[neighbour_subset[1][0]:neighbour_subset[1][1]]:
+
+				# If cell == 1, note as an additional neighbour
 				if cell == 1:
 					neighbours += 1
 
+		# Iteration counts current cell, so subject to account for this
+		# if necessary
 		if state[current[0]][current[1]] == 1:
 			neighbours -= 1
 
+
+		# Return evolved state as defined by scenarios
+
+		# Scenarios 1:3
 		if state[current[0]][current[1]] == 1:
+			# Scenario 1,2
 			if neighbours < 2 or neighbours > 3:
 				return 0
+			# Scenario 3
+			else:
+				return 1
+		# Scenarios 0,4,5
 		else:
+			# Scenario 4
 			if neighbours == 3:
 				return 1
+			# Scenario 0,5
+			else:
+				return 0
--- tests/testlife.py
+++ tests/testlife.py
@@ -9,3 +9,55 @@ class testLife(unittest.TestCase):
 		testLife = Life()
 		self.assertEqual(emptyState, testLife.evolve(emptyState))
 
+	def test_underpopulation(self):
+		startState = [ [1,0,0],
+					   [0,0,0],
+					   [0,0,1] ]
+
+		endState =   [ [0,0,0],
+					   [0,0,0],
+					   [0,0,0] ]
+
+		testLife = Life()
+		self.assertEqual(endState, testLife.evolve(startState))
+
+	def test_overcrowding(self):
+		startState = [ [1,1,1],
+		 			   [1,1,1],
+					   [1,1,1] ]
+
+		endState =   [ [1,0,1],
+					   [0,0,0],
+					   [1,0,1] ]
+
+		testLife = Life()
+		self.assertEqual(endState, testLife.evolve(startState))
+
+	def test_survival(self):
+		startState = [ [0,1,0],
+					   [1,0,1],
+					   [0,1,0] ]
+
+		endState =   [ [0,1,0],
+					   [1,0,1],
+					   [0,1,0] ]
+
+		testLife = Life()
+		self.assertEqual(endState, testLife.evolve(startState))
+
+	def test_seeded_outcome(self):
+		startState = [ [0,0,0,0,0],
+		 			   [0,0,0,0,0],
+					   [0,1,1,1,0],
+					   [0,0,0,0,0],
+					   [0,0,0,0,0] ]
+
+		nextState =  [ [0,0,0,0,0],
+		 			   [0,0,1,0,0],
+					   [0,0,1,0,0],
+					   [0,0,1,0,0],
+					   [0,0,0,0,0] ]
+
+		testLife = Life()
+		self.assertEqual(nextState, testLife.evolve(startState))
+		self.assertEqual(startState, testLife.evolve(nextState))
